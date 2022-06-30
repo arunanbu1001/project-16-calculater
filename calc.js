6e34c5d477289759445081fb122cd898322bb74a
arunanbu1001 <arunanbu10012001@gmail.com>
2022-06-30 14:50:54 +0530

    add

---
+++ calc.js
@@ -0,0 +1,58 @@
+function one(){
+ let x =document.getElementById('one1').value=1;
+ let d=document.getElementById('textvalue').innerHTML+=x;
+ console.log(d);
+}
+function two(){
+    let x =document.getElementById('two1').value=2;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function three(){
+    let x =document.getElementById('one1').value=3;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function four(){
+    let x =document.getElementById('four1').value=4;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function five(){
+    let x =document.getElementById('five1').value=5;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function six(){
+    let x =document.getElementById('six1').value=6;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function seven(){
+    let x =document.getElementById('seven1').value=7;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function eight(){
+    let x =document.getElementById('eight1').value=8;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function nine(){
+    let x =document.getElementById('nine1').value=9;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function zero(){
+    let x =document.getElementById('zero1').value=0;
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+
+function reset(){
+    document.getElementById('textvalue').innerHTML='';
+}
+
+    
+
+
