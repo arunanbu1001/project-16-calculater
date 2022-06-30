5154787e3a50b9a60fc1ffb493705b834e412692
arunanbu1001 <arunanbu10012001@gmail.com>
2022-06-30 16:06:39 +0530

    update

--- calc.js
+++ calc.js
@@ -1,50 +1,50 @@
 function one(){
- let x =document.getElementById('one1').value=1;
+    let x =document.getElementsByClassName('number').value=1;
  let d=document.getElementById('textvalue').innerHTML+=x;
  console.log(d);
 }
 function two(){
-    let x =document.getElementById('two1').value=2;
+    let x =document.getElementsByClassName('number').value=2;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function three(){
-    let x =document.getElementById('one1').value=3;
+    let x =document.getElementsByClassName('number').value=3;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function four(){
-    let x =document.getElementById('four1').value=4;
+    let x =document.getElementsByClassName('number').value=4;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function five(){
-    let x =document.getElementById('five1').value=5;
+    let x =document.getElementsByClassName('number').value=5;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function six(){
-    let x =document.getElementById('six1').value=6;
+    let x =document.getElementsByClassName('number').value=6;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function seven(){
-    let x =document.getElementById('seven1').value=7;
+    let x =document.getElementsByClassName('number').value=7;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function eight(){
-    let x =document.getElementById('eight1').value=8;
+    let x =document.getElementsByClassName('number').value=8;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function nine(){
-    let x =document.getElementById('nine1').value=9;
+    let x =document.getElementsByClassName('number').value=9;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
    function zero(){
-    let x =document.getElementById('zero1').value=0;
+    let x =document.getElementsByClassName('number').value=0;
     let d=document.getElementById('textvalue').innerHTML+=x;
     console.log(d);
    }
@@ -52,7 +52,31 @@ function two(){
 function reset(){
     document.getElementById('textvalue').innerHTML='';
 }
-
+function division(){
+    let x =document.getElementsByClassName('operation').value='/';
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function multiplication(){
+    let x =document.getElementsByClassName('operation').value='*';
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function subtraction(){
+    let x =document.getElementsByClassName('operation').value='-';
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function add(){
+    let x =document.getElementsByClassName('operation').value='+';
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
+   function equal(){
+    let x =document.getElementsByClassName('operation').value='=';
+    let d=document.getElementById('textvalue').innerHTML+=x;
+    console.log(d);
+   }
     
 
 
